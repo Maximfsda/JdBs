9d4a9af19445501c1ab0afa3c209e4ee128d06c3
Maximfsda <lisitsin.maxi@yandex.ru>
2023-03-01 21:42:04 +0700

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+public class Application {public static void main(String[] args) {
+    try (Connection connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/skypro", "postgres", "13579Tetris")) {
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT * FROM employee WHERE id = 1");{
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(
+                        "id = " + resultSet.getInt("id") +"\n"+
+                                "name = " + resultSet.getString("firstname")+"\n"+
+                                "lastname = " + resultSet.getString("lastname")+"\n"+
+                                "gender = " + resultSet.getString("gender")+"\n"+
+                                "age = " + resultSet.getInt("age")
+
+
+                );
+
+            }
+
+        }
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+}
+}
