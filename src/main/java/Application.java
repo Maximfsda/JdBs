471979fb81f9e2eaf47bb4da498d40fa19b27744
Maximfsda <lisitsin.maxi@yandex.ru>
2023-03-05 03:48:31 +0700

    TaskTwo

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,28 +1,26 @@
 import java.sql.*;
 
-public class Application {public static void main(String[] args) {
-    try (Connection connection = DriverManager.getConnection(
-            "jdbc:postgresql://localhost:5432/skypro", "postgres", "13579Tetris")) {
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "SELECT * FROM employee WHERE id = 1");{
-            ResultSet resultSet = preparedStatement.executeQuery();
+import jdbc.ConnectionManager;
 
+public class Application {
+    public static void main(String[] args) {
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT * FROM employee WHERE id = 1");
+             ResultSet resultSet = preparedStatement.executeQuery()) {
             while (resultSet.next()) {
                 System.out.println(
-                        "id = " + resultSet.getInt("id") +"\n"+
-                                "name = " + resultSet.getString("firstname")+"\n"+
-                                "lastname = " + resultSet.getString("lastname")+"\n"+
-                                "gender = " + resultSet.getString("gender")+"\n"+
+                        "id = " + resultSet.getInt("id") + "\n" +
+                                "name = " + resultSet.getString("firstname") + "\n" +
+                                "lastname = " + resultSet.getString("lastname") + "\n" +
+                                "gender = " + resultSet.getString("gender") + "\n" +
                                 "age = " + resultSet.getInt("age")
-
-
                 );
-
             }
-
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-    } catch (SQLException e) {
-        throw new RuntimeException(e);
     }
 }
-}
+
+
---
+++ src/main/java/dao/CityDao.java
@@ -0,0 +1,10 @@
+package dao;
+
+import model.City;
+
+import java.util.Optional;
+
+public interface CityDao {
+
+    Optional<City> findById(long id);
+}
